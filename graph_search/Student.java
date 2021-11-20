fb23e53bea900e269f590b6d90c5b5f6e779177e
Jake Keller <jakekeller321@gmail.com>
2021-11-20 16:09:03 -0500

    Created a student text file and some methods for the student class.
    Student text file holds all of the data that will represent a student. The methods created will read from the text file to create a student object.

--- graph_search/Student.java
+++ graph_search/Student.java
@@ -14,8 +14,9 @@ public class Student {
 
     private String name; // Holds name of student
     private String student_num; // Holds the student number of the student
-    private ArrayList<String> classes; // Holds the classes already taken by the student
-    private ArrayList<String> grades; // Holds the grades for each class
+    private ArrayList<String> classes = new ArrayList<String>(); // Holds the classes already taken by the student
+    private ArrayList<Integer> class_weights = new ArrayList<Integer>(); // Holds the weight for each class taken
+    private ArrayList<String> grades = new ArrayList<String>(); // Holds the grades for each class
 
     /**
      * Basic Constructor method. Reads from a provided filename that holds the
@@ -25,6 +26,66 @@ public class Student {
      *                 contains all of there information.
      */
     public Student(String filename) {
+
+        // Create the file object using the filename
+        File inputFile = new File(filename);
+        // Try to open and read from file
+        try {
+            // Create scanner to read from file
+            Scanner input = new Scanner(inputFile);
+
+            // Get the first two lines of the file for name and std_num
+            this.name = input.nextLine();
+            this.student_num = input.nextLine();
+
+            // Cycle through the file until the end
+            while (input.hasNext()) {
+                String line = input.nextLine(); // Get line
+                String[] arr = line.split(","); // Split each line by ','
+
+                // Load in each value of the line to the arraylists
+                classes.add(arr[0]);
+                class_weights.add(Integer.parseInt(arr[1]));
+                grades.add(arr[2]);
+
+            }
+            // Close the open file
+            input.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("");
+        }
+
+    }
+
+    /**
+     * toString method to display all information on the student.
+     */
+    public String toString() {
+
+        String ret_str = "";
+
+        ret_str += "Name:\t" + this.name + "\n";
+        ret_str += "Student Number:\t" + this.student_num + "\n";
+        ret_str += "------------------------------";
+
+        return ret_str;
+    }
+
+    /**
+     * Calculates the current GPA based on the class weights and the grades recieved
+     * 
+     * @return STring representing the students GPA otu of 4.0
+     */
+    public String getGPA() {
+        return "";
+    }
+
+    /**
+     * Driver function
+     * 
+     * @param args command line arguments
+     */
+    public static void main(String args[]) {
 
     }
 
